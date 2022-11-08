bd77f733e6afcd09497a668e4e6371330c6f8d07
Ahmed <amd.murtada@gmail.com>
2022-11-08 15:04:54 +0400

    Added Consumer and Producer projects

---
+++ Consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+channel.QueueDeclare(queue: "letterbox"
+, durable: false
+, exclusive: false
+, autoDelete: false
+, arguments: null
+);
+
+
+var consumer = new EventingBasicConsumer(channel);
+int consumedMessages = 0;
+
+consumer.Received += (model, ea) => {
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    consumedMessages++;
+    Console.WriteLine($"Message Received: {message}, Consumed Message: {consumedMessages}");
+};
+
+channel.BasicConsume(queue: "letterbox", autoAck: true, consumer: consumer);
+
+Console.ReadKey();
---
+++ Producer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using RabbitMQ.Client;
+
+
+string queue = "letterbox";
+var message = "Message ";
+int msgNumber = 100000;
+var stopWatch = new Stopwatch();
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+
+stopWatch.Start();
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+channel.QueueDeclare(queue: "letterbox"
+, durable: false
+, exclusive: false
+, autoDelete: false
+, arguments: null
+);
+
+Parallel.For(0, msgNumber, (i) =>
+//for(int i=0; i < msgNumber; i++)
+{
+    var msg = message + $"#{i}";
+    var encodedMessage = Encoding.UTF8.GetBytes(msg);
+
+    channel.BasicPublish("", queue, null, encodedMessage);
+    Console.WriteLine($"Message {msg} Pushed, duration: {stopWatch.ElapsedMilliseconds}");
+});
+stopWatch.Stop();
+
+Console.WriteLine($"Published Message {message}, duration: {stopWatch.ElapsedMilliseconds}");
